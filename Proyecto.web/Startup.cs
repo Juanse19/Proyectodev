6c80166d77f4617a923ab2cd37cbd14ab413b093
sebastian losada <sebastian97-19@hotmail.com>
2020-09-14 16:47:38 +0000

    Solucion de prueba

---
+++ Proyecto.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto.web.Startup))]
+namespace Proyecto.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
